37df46157771bb07f2f08e428b32a4938bf0239d
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-17 20:35:47 +0530

    feat: implement user registration controller with bcrypt and resend email config

---
+++ controllers/user_controller.js
@@ -0,0 +1,52 @@
+import sendEmail from '../config/sendEmail.js'
+import UserModel from '../models/user_model.js'
+import bcryptjs from 'bcryptjs'
+
+export async function registerUserController(request,response) {
+    try {
+        const { name, email, password } =request.body
+
+        if(!name || !email || !password){
+            return response.status(400).json({
+                message : "provide email, name, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(user){
+            return response.json({
+                message : "Already register email",
+                error : true,
+                success : false
+            })
+        }
+
+    const salt = await bcryptjs.genSalt(10)
+    const hashPassword = await bcryptjs.hash(password,salt)
+
+    const payload = {
+        name,
+        email,
+        password : hashPassword
+    }
+
+    const newUser = new UserModel(payload)
+    const save = await newUser.save()
+
+    const verifyEmail = await sendEmail({
+        sendTo : email,
+        subject : "Verify email from binkeyit"
+ 
+    })
+
+    } catch (error){
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+} 
