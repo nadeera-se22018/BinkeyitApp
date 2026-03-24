f0fdce66b542703551ca71047c44a6683d3204a9
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-24 18:02:26 +0530

    feat: add routes for user login and email verification

--- route/user_route.js
+++ route/user_route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { registerUserController } from '../controllers/user_controller.js'
+import { loginController,registerUserController, verifyEmailController } from '../controllers/user_controller.js'
 
 const userRouter = Router()
 
 userRouter.post('/register',registerUserController)
+userRouter.post('/verifyemail',verifyEmailController)
+userRouter.post('/login',loginController)
 
 export default userRouter
