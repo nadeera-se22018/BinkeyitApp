f4f029dd93464d5731810be8ecb13d3106b8884f
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-24 17:46:45 +0530

    style: update verify email template with improved button ui

--- utils/verifyEmailTemplate.js
+++ utils/verifyEmailTemplate.js
@@ -1,5 +1,5 @@
-const verifyEmailTemplate = ({name, url})=>{
-    return`
+const verifyEmailTemplate = ({name,url})=>{
+        return`
     <p>Dear ${name}</p>
     <p>Thank you for registering Binkeyit.</p>
     <a href=${url} style="color:white;background : blue;margin-top : 10px">
