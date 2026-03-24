16939c4fad4e55ff9188180bcb1cabedee61ff04
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-24 17:57:29 +0530

    feat: implement user login controller with jwt cookies

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,6 +2,8 @@ import sendEmail from '../config/sendEmail.js'
 import UserModel from '../models/user_model.js'
 import bcryptjs from 'bcryptjs'
 import verifyEmailTemplate from '../utils/verifyEmailTemplate.js'
+import generatedAccessToken from '../utils/generatedAccessToken.js'
+import generatedRefreshToken from '../utils/generatedRefreshToken.js'
 
 export async function registerUserController(request,response) {
     try {
@@ -63,3 +65,111 @@ export async function registerUserController(request,response) {
         })
     }
 } 
+
+export async function verifyEmailController(request,response){
+    try {
+        const { code } = request.body
+
+        const user = await UserModel.findOne({ _id : code})
+
+        if(!user){
+            return response.status(400).json({
+                message : "Invalid code",
+                error : true,
+                success : false
+            })
+        }
+
+        const updateUser = await UserModel.updateOne({ _id : code },{
+            verify_email : true
+        })
+
+        return response.json({
+            message : "Verify email done",
+            success : true,
+            error : false
+        })
+    } catch (error) {
+        return response .status(500).json({
+            message : error.message || error,
+            error : true,
+            success : true
+        })
+    }
+}
+
+//login controller
+export async function loginController(request,response) {
+    try{
+        const { email , password} = request.body
+
+
+        if(!email || !password){
+            return response.status(400).json({
+                message : "provide email, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "User not register",
+                error : true,
+                success : false
+            })
+        }
+
+        if(user.status !== "Active"){
+            return response.status(400).json({
+                message : "Contact to Admin",
+                error : true,
+                success : false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password,user.password)
+
+        if(!checkPassword){
+            return response.status(400).json({
+                message : "Check your password",
+                error : true,
+                success : false
+            })
+        }
+
+        const accesstoken = await generatedAccessToken(user._id)
+        const refreshToken = await generatedRefreshToken(user._id)
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        response.cookie('accessToken',accesstoken,cookiesOption)
+        response.cookie('refreshToken',refreshToken,cookiesOption)
+
+        return response.json({
+            message : "Login successfully",
+            error : false,
+            success : true,
+            data : {
+                accesstoken,
+                refreshToken
+            }
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+
+
+
+
+}
