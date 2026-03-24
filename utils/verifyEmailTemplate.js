187a09c2c33ad0adddc275639e19f0b724905dad
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-24 18:06:34 +0530

    style: fix inline css syntax and update button styling in email template

--- utils/verifyEmailTemplate.js
+++ utils/verifyEmailTemplate.js
@@ -2,7 +2,7 @@ const verifyEmailTemplate = ({name,url})=>{
         return`
     <p>Dear ${name}</p>
     <p>Thank you for registering Binkeyit.</p>
-    <a href=${url} style="color:white;background : blue;margin-top : 10px">
+    <a href=${url} style="color:white;background : #071263;margin-top : 10px,padding:20px,display:block">
         Verify Email
     </a>
     `
