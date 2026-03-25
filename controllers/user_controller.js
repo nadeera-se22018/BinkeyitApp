bf00466fee3feb3e74ca21b9fccd9b0e7c847b23
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-26 02:53:26 +0530

    fix: resolve refresh token deletion issue in logout controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -168,8 +168,36 @@ export async function loginController(request,response) {
             success : false
         })
     }
+}
 
+//logout controller
+export async function logoutController(request,response) {
+    try {
+        const userid = request.userId //middleware
 
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
 
+        response.clearCookie("accessToken",cookiesOption)
+        response.clearCookie("refreshToken",cookiesOption)
 
+        const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
+            refresh_token : ""
+        })
+
+        return response.json({
+            message : "Logout successfully",
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
 }
