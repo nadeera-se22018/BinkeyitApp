5baa5890ca2a1221f85c41e4befefeb42bf537fe
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-26 02:44:10 +0530

    feat: add secure logout route with auth middleware integration

--- route/user_route.js
+++ route/user_route.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { loginController,registerUserController, verifyEmailController } from '../controllers/user_controller.js'
+import { loginController,logoutController,registerUserController, verifyEmailController } from '../controllers/user_controller.js'
+import auth from '../middleware/auth.js'
 
 const userRouter = Router()
 
 userRouter.post('/register',registerUserController)
 userRouter.post('/verifyemail',verifyEmailController)
 userRouter.post('/login',loginController)
+userRouter.get('/logout',auth,logoutController)
 
 export default userRouter
