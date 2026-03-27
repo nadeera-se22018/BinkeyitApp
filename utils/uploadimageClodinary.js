a91b84087bceed9d6d3a4e38913d976be307ecfc
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-03-28 02:38:12 +0530

    feat: integrate cloudinary for secure image uploads

---
+++ utils/uploadimageClodinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+dotenv.config();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadImageClodinary = async(image) => { 
+    
+    const buffer = image?.buffer || Buffer.from(await image.arrayBuffer());
+
+    const updateImage = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({ folder : "binkeyit" }, (error, uploadResult) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(uploadResult);
+        }).end(buffer);
+    });
+
+    return updateImage;
+}
+
+export default uploadImageClodinary;
