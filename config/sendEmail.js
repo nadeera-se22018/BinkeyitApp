5982a662894227e4699bd57931db7a883f1f6992
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-04-24 18:39:11 +0530

    refactor: clean up sendEmail utility and refine error handling

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -8,23 +8,25 @@ if(!process.env.RESEND_API){
 
 const resend = new Resend(process.env.RESEND_API);
 
-const sendEmail = async({ name, sendTo,subject,html })=>{
+const sendEmail = async({ name, sendTo, subject, html })=>{
     try {
-    const { data, error } = await resend.emails.send({
-        from: 'Binkeyit <onboarding@resend.dev>',
-        to: sendTo,
-        subject: subject,
-        html: html,
-  });
+        const { data, error } = await resend.emails.send({
+            from: 'Binkeyit <onboarding@resend.dev>',
+            to: sendTo,
+            subject: subject,
+            html: html,
+        });
 
-    if (error) {
-        return console.error({ error });
-    }
+        if (error) {
+            return null; 
+        }
 
-        return data
+        return data;
+        
     } catch (error){
-        console.log(error)
+        console.error("Catch Block Error:", error);
+        return null;
     }
 }
 
-export default sendEmail
+export default sendEmail;
